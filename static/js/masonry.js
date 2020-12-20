2c775b83bae8913b0e3a016e831f6f85d828ef39
Richard Chin <richardchin@richards-air.lan>
2020-12-20 11:26:05 -0500

    mobile styling

--- static/js/masonry.js
+++ static/js/masonry.js
@@ -1,8 +1,8 @@
 var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
-// options
-itemSelector: '.grid-item',
-columnWidth: 200
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
 });
 
 // element argument can be a selector string

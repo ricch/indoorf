f96c31b8562f04b636887cc07232ff1e96ef4f3b
Richard Chin <richardchin@richards-air.lan>
2020-12-20 08:59:41 -0500

    first init

---
+++ content/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Home"
+date: 2020-12-18T21:57:35-05:00
+description: ""
+---
+
+hello world
---
+++ static/js/scroll-reveal.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.card', { 
+    easing: 'ease-in-out', 
+    duration: '500', 
+    delay: '600',
+    distance: '50px',
+    origin: 'bottom'
+});
